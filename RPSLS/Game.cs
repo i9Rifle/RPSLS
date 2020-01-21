b729fdb03904fd4ea4766d25ff781c6646d66469
Matthew Acker <matthew.acker11b@gmail.com>
2020-01-21 09:29:50 -0600

    added UI

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -11,32 +11,8 @@ namespace RPSLS
         Player player1;
         Player player2;
 
-
-        public void WelcomeMessage()
-        {
-            Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
-            Console.WriteLine("Classic Rock, Paper, Scissors with a twist!");
-            Console.WriteLine("--------------------------------------------------");
-            Console.WriteLine("Play single player (human  vs CPU) or multiplayer (human vs human)");
-            Console.WriteLine("User can enter their own custom names");
-            Console.WriteLine("Each match will be a 'best of three' series");
-            Console.WriteLine("--------------------------------------------------------------------");
-            Console.WriteLine("Rules: What Beats What?");
-            Console.WriteLine("Rock crushes Scissors");
-            Console.WriteLine("Scissors cuts Paper");
-            Console.WriteLine("Paper covers Rock");
-            Console.WriteLine("Rock crushes Lizard");
-            Console.WriteLine("Lizard poisons Spock");
-            Console.WriteLine("Spock smashes Scissors");
-            Console.WriteLine("Scissors decapitates Lizard");
-            Console.WriteLine("Lizard eats Paper");
-            Console.WriteLine("Paper disproves Spock");
-            Console.WriteLine("Spock vaporzes Rock");
-            Console.WriteLine("-----------------------------");
-        }
         public void RunGame() // master method
         {
-            WelcomeMessage();
             string numberOfPlayers = ChoseNumberOfPlayers();
             SetPlayers(numberOfPlayers);
 
